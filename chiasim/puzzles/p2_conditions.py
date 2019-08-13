eb3683e39a182f153a89b4359b7b155e7722abda
Richard Kiss <him@richardkiss.com>
2019-08-13 12:00:55 -0700

    First puzzle ported to "contract".

--- chiasim/puzzles/p2_conditions.py
+++ chiasim/puzzles/p2_conditions.py
@@ -17,9 +17,43 @@ from opacity import binutils
 from chiasim.hashable import Program
 
 
+# contract:
+# generate puzzle: (() . puzzle_parameters)
+# generate solution: (1 . (puzzle_parameters . solution_parameters))
+
+
+def make_contract():
+    """
+    Rough source (hasn't been tested):
+        (mod (is_solution . parameters)
+            (defmacro puzzle conditions (qq (q (unquote conditions))))
+            (defmacro solution (conditions . solution_info) (qq (q (unquote (c conditions ())))))
+            (if is_solution (solution parameters) (puzzle parameters))
+        )
+    """
+    return binutils.assemble("""
+    (e (i (f (a))
+       (q (c (c (q #q) (c (f (r (a)))
+       (q ()))) (q (())))) (q (c (q #q) (c (r (a)) (q ()))))) (a))
+    """)
+
+
+CONTRACT = make_contract()
+
+
+def puzzle_for_contract(contract, puzzle_parameters):
+    return Program(clvm.eval_f(clvm.eval_f, contract, clvm.to_sexp_f(
+        []).cons(puzzle_parameters)))
+
+
+def solution_for_contract(contract, puzzle_parameters, solution_parameters):
+    return Program(clvm.eval_f(clvm.eval_f, contract, clvm.to_sexp_f(
+        (1, (puzzle_parameters, solution_parameters)))))
+
+
 def puzzle_for_conditions(conditions):
-    return Program(clvm.to_sexp_f([binutils.assemble("#q"), conditions]))
+    return puzzle_for_contract(CONTRACT, conditions)
 
 
 def solution_for_conditions(conditions):
-    return Program(clvm.to_sexp_f([puzzle_for_conditions(conditions).code, []]))
+    return solution_for_contract(CONTRACT, conditions, [])
