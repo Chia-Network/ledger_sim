53fc036324d8350a02306184bb326d1adbff7fe0
Richard Kiss <him@richardkiss.com>
2019-09-16 16:49:42 -0700

    Make Program a subclass of SExp.

--- chiasim/hashable/Program.py
+++ chiasim/hashable/Program.py
@@ -8,19 +8,34 @@ from ..atoms import hash_pointer, streamable
 from ..atoms.bin_methods import bin_methods
 
 
-@streamable
-class Program(bin_methods):
+SExp = to_sexp_f(1).__class__
+
+
+class Program(SExp):
     """
     A thin wrapper around s-expression data intended to be invoked with "eval".
     """
     code: BaseSExp
 
+    def __init__(self, v):
+        if isinstance(v, SExp):
+            v = v.v
+        super(Program, self).__init__(v)
+        self.code = self
+
     @classmethod
     def parse(cls, f):
-        return cls(sexp_from_stream(f, to_sexp_f))
+        return sexp_from_stream(f, cls.to)
 
     def stream(self, f):
-        sexp_to_stream(self.code, f)
+        sexp_to_stream(self, f)
+
+    def __getattribute__(self, attr_name):
+        if attr_name == "code":
+            # breakpoint()
+            # uncomment the line above to find where ".code" is used
+            return self
+        return object.__getattribute__(self, attr_name)
 
 
 ProgramHash = hash_pointer(Program, std_hash)
