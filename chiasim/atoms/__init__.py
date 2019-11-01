1d4794f17709cf19ad40f9eb2afa81b079328228
Richard Kiss <him@richardkiss.com>
2019-11-01 12:26:45 -0700

    Dedup byte_types.

--- chiasim/atoms/__init__.py
+++ chiasim/atoms/__init__.py
@@ -1,6 +1,6 @@
 import dataclasses
 
-from .byte_types import make_sized_bytes  # noqa
+from src.util.byte_types import make_sized_bytes  # noqa
 from .hash_pointer import hash_pointer  # noqa
 from .hexbytes import hexbytes  # noqa
 from src.util.ints import int8, uint8, int16, uint16, int32, uint32, int64, uint64  # noqa
