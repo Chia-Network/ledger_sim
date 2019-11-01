6789db0a736226f34233f01f73f2b45720e8a270
Richard Kiss <him@richardkiss.com>
2019-11-01 14:50:43 -0700

    Remove bin_methods.

--- chiasim/atoms/hash_pointer.py
+++ chiasim/atoms/hash_pointer.py
@@ -1,5 +1,3 @@
-from .bin_methods import bin_methods
-
 from typing import get_type_hints
 
 
@@ -8,7 +6,7 @@ def hash_pointer(the_type, hash_f):
     Create a "cryptographic pointer" type that can accept either a hash or an instance
     of the type. It can also reconstruct the underlying the object given a data source.
 
-    The resulting type subclasses "bin_methods", and can stream and parse itself.
+    The resulting type subclasses the type returned from hash_f.
     """
     hash_type = get_type_hints(hash_f)["return"]
 
@@ -38,5 +36,5 @@ def hash_pointer(the_type, hash_f):
 
     namespace = dict(__new__=__new__, obj=obj)
     hash_pointer_type = type(
-        "%sPointer" % the_type.__name__, (hash_type, bin_methods,), namespace)
+        "%sPointer" % the_type.__name__, (hash_type,), namespace)
     return hash_pointer_type
