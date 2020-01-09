45f1af4f320e1932005e6da509c56642a12848d4
Richard Kiss <him@richardkiss.com>
2020-01-09 13:05:28 -0800

    Get tests passing.

--- chiasim/atoms/__init__.py
+++ chiasim/atoms/__init__.py
@@ -1,5 +1,7 @@
 import dataclasses
 
+from .bin_methods import bin_methods
+
 from src.util.byte_types import make_sized_bytes  # noqa
 from .hash_pointer import hash_pointer  # noqa
 from src.util.ints import int8, uint8, int16, uint16, int32, uint32, int64, uint64  # noqa
