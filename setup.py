ae9ea19f68db6b716521ec867a5eb7a6623ccafc
Richard Kiss <him@richardkiss.com>
2020-01-09 15:20:26 -0800

    Compile clvm into clvm.hex files in setup.

--- setup.py
+++ setup.py
@@ -5,11 +5,14 @@ https://packaging.python.org/guides/distributing-packages-using-setuptools/
 https://github.com/pypa/sampleproject
 """
 
-# Always prefer setuptools over distutils
+from clvm_tools.setuptools import build_clvm, monkey_patch
 from setuptools import setup, find_packages
 from os import path
 
+monkey_patch()
+
 here = path.abspath(path.dirname(__file__))
+
 
 # Get the long description from the README file
 with open(path.join(here, "README.org"), encoding="utf-8") as f:
@@ -38,10 +41,24 @@ setup(
     setup_requires=["clvm_tools", "setuptools_scm"],
     use_scm_version=True,
     install_requires=[],
-    extras_require={"dev": [], "test": ["coverage"],},
-    entry_points={"console_scripts": ["ledger-sim=chiasim.cmds.ledger_sim:main",],},
+    extras_require={"dev": [], "test": ["coverage"], },
+    entry_points={"console_scripts": ["ledger-sim=chiasim.cmds.ledger_sim:main", ], },
     project_urls={
         "Bug Reports": "https://github.com/Chia-Network/ledger_sim",
         "Source": "https://github.com/Chia-Network/ledger_sim",
     },
+    clvm_extensions=[
+        "chiasim/puzzles/make_p2_delegated_puzzle_or_hidden_puzzle.clvm",
+        "chiasim/puzzles/make_puzzle_m_of_n_direct.clvm",
+    ],
+    data_files=[
+        (
+            "chiasim/puzzles",
+            [
+                "make_p2_delegated_puzzle_or_hidden_puzzle.clvm.hex",
+                "chiasim/puzzles/make_puzzle_m_of_n_direct.clvm.hex",
+            ],
+        )
+    ],
+    cmdclass={"build_clvm": build_clvm, },
 )
